c2121244a5160b288352d938ba9ef4eedf36f9db
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-11-01 15:16:25 +0600

    Update Where's_the_Marble.cpp

--- Where's_the_Marble.cpp
+++ Where's_the_Marble.cpp
@@ -20,3 +20,13 @@ int main() {
 	}
 	return 0;
 }
+
+/** By [Vector]:
+                        vector <int>::iterator indx = lower_bound(v.begin(), v.end(), iteam);
+                        int pos = indx - v.begin();
+                        if (indx == v.end() || *indx != iteam) printf("%d not found\n", iteam);
+                        else printf("%d found at %ld\n", iteam, pos+1);
+For Using [set]: you've to just count distance, caus figur out that, other you can acces set getting error....
+ 	
+		********auto it = distance(s.begin(), indx);
+*/
