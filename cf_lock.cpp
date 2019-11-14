3a51c42c90742f394a59084a2c261bb4830ec534
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-11-14 23:53:43 +0600

    Update cf_lock.cpp

--- cf_lock.cpp
+++ cf_lock.cpp
@@ -1,3 +1,18 @@
+/*https://codeforces.com/contest/540/problem/A
+
+here: min((a[i]-b[i]+10)%10,(b[i]-a[i]+10)%10) is the main logic you know :)
+// (a[i] - b[i] + 10) % 10 is meaning moving on accenfing form(0 to 9)
+// (b[i] - a[i] + 10) % 10 is meaning moving on decending form(9 to 0)
+Examples;
+Input:
+5
+82195
+64723
+output: 
+12
+here // at position 3, we have (1--->7) {1-7=-6+10=4%10=4 AND 7-1=6+10=16%10=6} so the minimum vakue 4 is taken...
+*/
+
 #include <iostream>
 using namespace std;
 int n,s,i;string a,b;
