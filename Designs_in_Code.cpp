a82ab114d605effed38ca2d494a42bb408d3a840
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-12-15 15:30:13 +0600

    Update Designs_in_Code.cpp

--- Designs_in_Code.cpp
+++ Designs_in_Code.cpp
@@ -1,3 +1,72 @@
+/*
+ *                             UM.
+ *                            J@B@1                                                                    iO@1
+ *                           Y@@@B@BB.                                                              7B@B@B@
+ *                          :@B@i,B@B@O                                                          ,Z@B@B@B@Br
+ *                          @B@q   i@B@BS                                                      7@B@@@O5vMB@q
+ *                         8@@B      LB@B@i                                                  FB@@@BNjYjLE@B@
+ *                        ,B@B:        0@@@Z                                               P@B@BM1JJ125JPB@B
+ *                        B@BB          :@B@B                                            XB@B@Z2LuU52F2u2@B@.
+ *                       :@B@             @@@B:                                        v@B@B8uJj51F1525uUB@B7
+ *                       @B@O              0@@B.               ..::ir7vvYUuU777r::.   B@B@OULU2F2F151F11Y@B@S
+ *                       B@B,               8B@B  :ruXMB@B@B@B@B@B@B@B@B@B@@@B@B@B@B@B@B@5Jj1211F1F1F2FUJO@BB
+ *                      U@B@                 @B@B@B@B@B@@@B@B@B@MMqPS5JuYL7rq@B@OBB@B@B8Yu211F1515251515YGB@@
+ *                      @B@u                 v@@@@MSur:.                    LB@MvvjJuU5YU252F1F1F25251F2uX@@@
+ *                      @@@.                                                N@BML2U2UUU12F15252525251515Jk@@B
+ *                     r@B@                                                 YB@Bju52121252515252F15251F2u5@B@
+ *                     PB@B                                                  @@@PYUF151F25151F152F2F1F15jF@@B
+ *                     @@BS                                                  N@@@UJ2F25252F251525151F1F1u5@B@
+ *                     @@@7                                                   B@B@5Yj12F152F1F1F25252515jFB@B
+ *                     B@Bi                                                    M@B@O2Luu52525212F151121UY1@B@7
+ *                    O@B@:                                                     v@B@BMSuYJJuuUu2u2uujjYJJXB@B@M
+ *                  7B@B@,                                                        1B@@@B@GPF1uujuu21PNMB@B@B@B@@
+ *                 qB@B2                                                            i8B@B@B@B@B@@@@@B@B@B@q: @@@B
+ *                MB@B:                                                                 7SBB@B@B@B@B@Zu:      @B@B
+ *               ZB@B.                                              ,v.                                        @B@L
+ *              LB@B,                         Y7                    @B@Bu                                      7@B@
+ *   :B@B@@B2:  @@B7                         @B@Z                   r@B@B@BP:                                   B@BE
+ *    BB@@@B@B@B@BE                        r@B@B                       7@B@B@B@Ou:                              iB@B
+ *         :uM@@B@@2.           :7::::ivk@B@B@0                           :5B@B@B@B@B@B@G.                       @B@i
+ *            BB@@@B@@         :@B@B@@@B@B@@1                                 .i5M@B@B@@@5                       M@@2
+ *            B@B ,@B1          L0EZZG0F7:                                            .:,                        uB@MrP@M7
+ *           2@B@                                                                                               ,O@B@B@B@B
+ *           @B@1                                                     :@B@@@r                                :@@@@B@BL:,,
+ *           B@Bi                         :2ZS;                      :@B@B@B@r                               L@B@B@BU
+ *           @B@.                        @@@B@B@                     vB@B@B@B5                                   @B@i
+ *           B@B                        7B@B@B@BM                     OB@B@B@                                   ,B@B
+ *           @B@                         @B@B@@@i                       rL7.                                    B@BM
+ *           B@B7.:                       NB@@M.                                                               .@B@.
+ *  .;JEB@@@B@B@B@B@.                                                                                       .  @B@u
+ *@@@B@B@B@B@@@B@18U                                                                                      :B@B@B@BU,
+ *7@BOui.    ,@@B                                                                                          SP@B@B@B@B@Or
+ *            @@@U                                                                                           B@BJ.YO@B@B@i
+ *            r@B@                                                                                         :B@Bk     .k@B@
+ *             B@B@                                                                                       LB@@k        2i
+ *              B@BM                                      .7jXEGqF7:                                     OB@@L
+ *              .B@BM                                   .B@B@B@B@B@B@.                                 :@B@B:
+ *               .B@B@                                   @@MYr::ivG@B                                .M@B@G
+ *                 B@@@S                                                                           ,MB@B@,
+ *                  v@@@BF                                                                      .1B@B@Br
+ *                    2@@B@BL                                                                ,FB@@@B8,
+ *                      r@B@B@BF,                                                        :YBB@B@B@B
+ *                         L@B@B@B@P7,                                           .ivXB@B@B@B@B@M@B@
+ *                            ,1B@B@B@B@@@BOP2L7i:,.              ..,:i7LSNB@@B@B@@@B@B@B@Z5v;.LB@@
+ *                              @B@OEB@B@@@B@B@B@B@B@B@B@B@@@B@B@B@B@B@@@B@B@B@B@BM0SJ7i::::i:,u@B@
+ *                              B@Bu ::i;7vu2XNGOMB@B@BMB@B@B@B@B@B@@@B1UFuj77ii:::::::iir;r;i.YB@B
+ *                              @B@L.:i:i:i::::::::::..Y@B@BMYi:i;SB@B@N:.::i:iirir;r;rii::::ivO@B@
+ *                              B@@X::,::::iirir;riri:E@B@1         ,@B@Br:;;r;rii:i::::i7JEB@@@@@B
+ *                              @@@B@BBq5v7ii:::::::.2@@@i  ..,..     @B@@,,:::::irv2XMB@B@B@B@2@B@:
+ *                             .B@BBB@@@B@B@B@BMNP5u7@B@1 .,,:,,  :.   @B@P50MB@B@B@B@B@@@BS:   @@B1
+ *                             E@B@   ijGB@B@B@B@B@B@B@Bi .,:,,..@@B@7 B@B@B@B@B@B@BM57.        kB@B
+ *                            .@B@:          .,ivu5Nq@B@u  ..,.. SB@B@@@B@PL7i,                 ,@B@
+ *                            @@@8                   i@B@:    .     :B@B@@                       B@@2
+ *                          i@@@@                     0@B@u          B@@B.                       vB@B
+ *                         ,@B@G                       L@B@BOv:.:iFB@B@M                          @B@Bi
+ *                          vNi                          S@@B@B@B@B@BM:                            MB@N
+ *                                                          758BMqJ,
+ */    
+
+
     //BINARY_BOY!!
     //THINK TWICE CODE ONCE { TRUE }
     // #####   ###  ##   #   ###   #####   #   #  #####	 ####    #   #
