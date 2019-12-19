1bd42a58cfc5baea17a904fd6ba0c59d3e4f57ea
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-12-19 12:14:16 +0600

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,3 +1,10 @@
+*** uva 10347 - Medians ***
+Very Easy Problem -----
+When Three Medians( মধ্যমা ) Are Given , Then Using Heron's Formula , We can solve the area : 
+				
+				Area = (4.0/3.0) * sqrt(s*(s-m1)*(s-m2)*(s-m3));	where , s = (m1+m2+m3) / 2;
+
+
 #include <iostream>
 #include <cmath>
 int main () {
