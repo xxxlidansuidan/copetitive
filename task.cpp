9ed63fc2164f4979eb06537ce129a2283bb9292c
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-12-19 17:04:04 +0600

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -12,7 +12,24 @@ Collateral  tasksmay as wellbeintroduced in discussion:circular permutation –s
 reduce the condition to sitting in any order, men sit together and women sit together;onecouple is arguing and they refuse to sit
 together, the other couples can sit in any way they want.
 
-Task 1 –Circus Night : (Answer)
+=>> Task 1 –Circus Night : (Answer) ==>>
 First, we consider the different ways of arranging the couples themselves, which is a permutation on nblocks.
 Second, within each couple, there two ways to seat individuals. Since there are ( n )couples, we multiply two ( n )times : 
               P(n, n)x 2 x 2 x ...... x 2 = ( n!.(2)^n );
+
+
+*** Task 2 –The BookShelf ***
+
+For a given number n, 0<n<11, generate all ways to arrange in a shelf the books coded with the set {1, 2, ..., n} in
+lexicographical order.
+
+Sample Input ------------------------------ Sample Output
+  3                                            1 2 3
+  2                                            1 3 2
+                                               2 1 3
+                                               2 3 1
+                                               3 1 2
+                                               3 2 1
+
+==>> Task 2 –The BookShelf ==>>
+There are lots of algorithms for permutations generation : next permutation, iterative and recursive ones. 
